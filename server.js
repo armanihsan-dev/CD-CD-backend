56b6b827af4972509d7c6f4947c31341d3ada2ae
user.email <armanihsan224@gmail.com>
2026-02-06 22:19:31 +0500

    update project

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { spawn } from 'child_process'
 
 const app = express();
 const PORT = 5000;
@@ -11,10 +12,49 @@ app.get("/", (req, res) => {
   res.send("🚀 Server running with ES6 Express");
 });
 
+app.post('/githubwebhook', (req, res) => {
+  console.log(req.headers);
+  console.log(req.body);
+
+  const bashChildProcess = spawn('bash', ['../deploy.sh'])
+  bashChildProcess.stdout.on('data', (data) => {
+    process.stdout.write(data)
+  })
+  // console.log(bashChildProcess.stderr);
+
+  bashChildProcess.on('close', (code) => {
+    if (code == 0) {
+      console.log('Script executed successfully');
+    } else {
+      console.log('Script execution failed with code: ' + code);
+    }
+    console.log(code);
+  })
+
+  bashChildProcess.on('error', (err) => {
+    console.error('Failed to start subprocess.', err);
+  })
+  res.status(200).json({ message: 'Webhook received' });
+})
+
+app.post('/hi', (req, res) => {
+  console.log('req is comming');
+  const { name, pass } = req.body
+  console.log(req.body);
+  res.status(200).json({ message: 'Hello from the other side', data: { name, pass } })
+})
+app.get('/fancy', (req, res) => {
+  res.status(200).json({ message: 'This is a fancy route' })
+})
 // api route
 app.get("/api", (req, res) => {
   res.json({
     message: "Hello from ES6 backend 🔥",
+    response: {
+      name: "John Doe",
+      age: 30,
+      email: `jhoan@gmail.com`
+    }
   });
 });
 
